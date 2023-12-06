7ecdd9d7c319d39b79c2cac65109cf6e10115f22
Ciro Gomes <142424957+cc23136@users.noreply.github.com>
2023-12-06 11:08:41 -0300

    Delete Hospital_View_DoctorSpec.sql

--- Hospital_View_DoctorSpec.sql
+++
@@ -1,5 +0,0 @@
-/* VIEW PARA A SELEÇÃO DOS MÉDICOS E SUAS RESPECTIVAS ESPECIALIZAÇÕES */
-CREATE OR ALTER VIEW V_DoctorSpec as
-SELECT Hospital.Doctor.nome + ' ' + Hospital.Doctor.sobrenome as nomeMedico, Hospital.Specialization.nomeEspec FROM
-	   Hospital.DoctorSpecialization INNER JOIN Hospital.Doctor on DoctorSpecialization.CRM = Doctor.CRM INNER JOIN 
-	   Hospital.Specialization on DoctorSpecialization.idEspecializacao = Specialization.idEspecializacao
