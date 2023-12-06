f9693170b5d43db992a8980dca1718804a3738cc
Ciro Gomes <142424957+cc23136@users.noreply.github.com>
2023-12-06 11:08:05 -0300

    Create Hospital_View_DoctorSpec.sql

---
+++ Hospital_View_DoctorSpec.sql
@@ -0,0 +1,5 @@
+/* VIEW PARA A SELEÇÃO DOS MÉDICOS E SUAS RESPECTIVAS ESPECIALIZAÇÕES */
+CREATE OR ALTER VIEW V_DoctorSpec as
+SELECT Hospital.Doctor.nome + ' ' + Hospital.Doctor.sobrenome as nomeMedico, Hospital.Specialization.nomeEspec FROM
+	   Hospital.DoctorSpecialization INNER JOIN Hospital.Doctor on DoctorSpecialization.CRM = Doctor.CRM INNER JOIN 
+	   Hospital.Specialization on DoctorSpecialization.idEspecializacao = Specialization.idEspecializacao
